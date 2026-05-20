26ab56ec42729a5c41de321343075df94db0c71d
MadhuMeghanaS <suddulamadhumeghana@gmail.com>
2026-05-20 18:56:08 +0530

    Updated project files

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Load external CSS
 with open('style.css', encoding='utf-8') as f:
@@ -30,7 +31,11 @@ def recommend(movie):
 
 movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+vectors = pickle.load(open('vectors.pkl', 'rb'))
+
+# calculating similarity
+cosine_similarity(vectors)
+similarity = cosine_similarity(vectors)
 
 st.title('Movie Recommender System')
 
